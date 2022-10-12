4a49707498838dcf445179ae33525c4bf4077bdf
Lubo K <lubomirkurcak@gmail.com>
2022-10-13 01:51:57 +0200

    comment unsued var

--- misc/assetlister.js
+++ misc/assetlister.js
@@ -8,7 +8,7 @@ for (const file of files) {
     const filepath = path.join(assetpath, file);
     const name = path.parse(filepath).name;
     const base = path.parse(filepath).base;
-    const ext = path.parse(filepath).ext;
+    // const ext = path.parse(filepath).ext;
 
     console.log(`{ name: '${name}', value: '${base}' },`);
 }
